2192fadf9c2019fe52df7d4ec5850a9beb64ee18
ibrahim sobh <isobh@outlook.com>
2022-05-02 23:38:08 +0200

    Step 3 and the begining of Step 4

---
+++ house_prices/inference.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
+def make_predictions(input_data: pd.DataFrame) -> np.ndarray:
+    # the model and all the data preparation objects (encoder, etc) should be loaded from the models folder
+    pass
---
+++ house_prices/train.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def build_model(data: pd.DataFrame) -> dict[str, str]:
+    # Returns a dictionary with the model performances (for example {'rmse': 0.18})
+    pass
