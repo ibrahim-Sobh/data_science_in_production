23551fe63eb37fcb6555d366c5527445f8444974
ibrahim sobh <isobh@outlook.com>
2022-05-03 02:07:20 +0200

    Step 4 Commmit

--- house_prices/inference.py
+++ house_prices/inference.py
@@ -1,11 +1,25 @@
-%matplotlib inline
-%load_ext autoreload
-%autoreload 2
-
 import pandas as pd
 import numpy as np
-
+from house_prices.preprocess import data_preprocessing
+from joblib import load
 
 def make_predictions(input_data: pd.DataFrame) -> np.ndarray:
-    # the model and all the data preparation objects (encoder, etc) should be loaded from the models folder
-    pass
+    
+    # load the encoder 
+    encoder_filename="../models/encoder.joblib" 
+    ordinal =load(encoder_filename)
+
+    # load the scalar
+    scalar_filename="../models/scalar.joblib" 
+    scalar=load(scalar_filename)
+
+    # load the model
+    model_filename="../models/model.joblib" 
+    model= load(model_filename)
+    
+    input_data=data_preprocessing(input_data,encoder=ordinal,scalar=scalar,is_test=True)
+    
+    #Validation-set evaluation
+    y_predictions=model.predict(input_data)
+    
+    return y_predictions
