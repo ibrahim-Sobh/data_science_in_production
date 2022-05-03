6c549ea37865559cdaae9a7beea7e5c2cb26d9d6
ibrahim sobh <isobh@outlook.com>
2022-05-03 12:40:46 +0200

    Step 4 and 5 finished

--- house_prices/inference.py
+++ house_prices/inference.py
@@ -3,23 +3,26 @@ import numpy as np
 from house_prices.preprocess import data_preprocessing
 from joblib import load
 
+ENCODER_PATH = "../models/encoder.joblib"
+SCALAR_PATH = "../models/scalar.joblib"
+MODEL_PATH = "../models/model.joblib"
+
+
 def make_predictions(input_data: pd.DataFrame) -> np.ndarray:
-    
-    # load the encoder 
-    encoder_filename="../models/encoder.joblib" 
-    ordinal =load(encoder_filename)
+
+    # load the encoder
+    ordinal = load(ENCODER_PATH)
 
     # load the scalar
-    scalar_filename="../models/scalar.joblib" 
-    scalar=load(scalar_filename)
+    scalar = load(SCALAR_PATH)
 
     # load the model
-    model_filename="../models/model.joblib" 
-    model= load(model_filename)
-    
-    input_data=data_preprocessing(input_data,encoder=ordinal,scalar=scalar,is_test=True)
-    
-    #Validation-set evaluation
-    y_predictions=model.predict(input_data)
-    
+    model = load(MODEL_PATH)
+
+    input_data = data_preprocessing(
+        input_data, encoder=ordinal, scalar=scalar, is_test=True)
+
+    # Validation-set evaluation
+    y_predictions = model.predict(input_data)
+
     return y_predictions
--- house_prices/preprocess.py
+++ house_prices/preprocess.py
@@ -2,60 +2,74 @@ import pandas as pd
 import numpy as np
 from joblib import dump
 
+ENCODER_PATH = "../models/encoder.joblib"
+SCALAR_PATH = "../models/scalar.joblib"
 
-ENCODER_PATH= "../models/encoder.joblib"
-SCALAR_PATH="../models/scalar.joblib"  
 
-def drop_unwanted_columns(data: pd.DataFrame, to_remove_columns: list=[]) -> pd.DataFrame:
-    return data.drop(to_remove_columns, axis = 1);
+def drop_columns(data: pd.DataFrame,
+                 to_remove_columns: list = []) -> pd.DataFrame:
+    return data.drop(to_remove_columns, axis=1)
 
-def encode_categorical_features(encoder,data: pd.DataFrame,is_test:bool =False) -> pd.DataFrame:
+
+def encode_categorical_features(encoder, data: pd.DataFrame,
+                                is_test: bool = False) -> pd.DataFrame:
     data_categorical = data.select_dtypes(include=['object']).columns
-    if not is_test :
+    if not is_test:
         encoder.fit(data[data_categorical])
-        dump(encoder ,ENCODER_PATH)
-    data[data_categorical]=encoder.transform(data[data_categorical])
-    return data;
+        dump(encoder, ENCODER_PATH)
+    data[data_categorical] = encoder.transform(data[data_categorical])
+    return data
+
 
 def fill_features_nulls(data: pd.DataFrame) -> pd.DataFrame:
-    
-    data_numerical= data.select_dtypes([np.int64,np.float64]).columns
+
+    data_numerical = data.select_dtypes([np.int64, np.float64]).columns
     data_categorical = data.select_dtypes(include=['object']).columns
 
-    data[data_numerical]=data[data_numerical].fillna(data[data_numerical].mean())
-    
-    for feature in data_categorical:
-        data[feature].interpolate(method ='linear', limit_direction ='forward', inplace=True)
-        data[feature].interpolate(method ='linear', limit_direction ='backward',inplace=True)
-        
-    return data;
+    data[data_numerical] = data[data_numerical].fillna(
+        data[data_numerical].mean())
 
-def scale_data(scalar,data: pd.DataFrame,is_test:bool =False) -> pd.DataFrame:
+    for feature in data_categorical:
+        data[feature].interpolate(
+            method='linear', limit_direction='forward', inplace=True)
+        data[feature].interpolate(
+            method='linear', limit_direction='backward', inplace=True)
+
+    return data
+
+
+def scale_data(scalar, data: pd.DataFrame,
+               is_test: bool = False) -> pd.DataFrame:
     if not is_test:
         scalar.fit(data)
-        dump(scalar ,SCALAR_PATH)
-    return pd.DataFrame(scalar.transform(data),columns = data.columns)
+        dump(scalar, SCALAR_PATH)
+    return pd.DataFrame(scalar.transform(data), columns=data.columns)
 
-def data_preprocessing(data: pd.DataFrame,encoder,scalar,is_test:bool=False) -> pd.DataFrame:  
-   
+
+def data_preprocessing(data: pd.DataFrame, encoder,
+                       scalar, is_test: bool = False) -> pd.DataFrame:
+
     # Carefully Selected Features (after analysis)
-    list_of_features =["OverallQual","GrLivArea","GarageCars","TotalBsmtSF","1stFlrSF",
-                       "FullBath","YearBuilt","YearRemodAdd","BsmtFinSF1","Foundation",
-                       "LotFrontage","WoodDeckSF","MasVnrArea","Fireplaces",
-                       "ExterQual","BsmtQual","KitchenQual","GarageFinish",
-                       "GarageType","HeatingQC"]
+    list_of_features = ["OverallQual", "GrLivArea", "GarageCars",
+                        "TotalBsmtSF", "1stFlrSF", "FullBath", "YearBuilt",
+                        "YearRemodAdd", "BsmtFinSF1", "Foundation",
+                        "LotFrontage", "WoodDeckSF", "MasVnrArea",
+                        "Fireplaces", "ExterQual", "BsmtQual",
+                        "KitchenQual", "GarageFinish", "GarageType",
+                        "HeatingQC"]
 
-    # To remove Columns 
-    unwanted_columns = ["PoolQC", "MiscFeature","Alley","Fence","FireplaceQu"]
-    
-    data= drop_unwanted_columns(data,to_remove_columns=unwanted_columns)
-    
-    data =data[list_of_features]
-    
-    data= encode_categorical_features(encoder,data,is_test)
-    
-    data= fill_features_nulls(data)
-    
-    data= scale_data(scalar,data,is_test)
-    
+    # To remove Columns
+    unwanted_columns = ["PoolQC", "MiscFeature",
+                        "Alley", "Fence", "FireplaceQu"]
+
+    data = drop_columns(data, to_remove_columns=unwanted_columns)
+
+    data = data[list_of_features]
+
+    data = encode_categorical_features(encoder, data, is_test)
+
+    data = fill_features_nulls(data)
+
+    data = scale_data(scalar, data, is_test)
+
     return data
